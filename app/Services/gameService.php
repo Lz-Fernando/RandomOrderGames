5b51f845974653d1509387bff006ae9a5fea66bd
lz-fernando <nandobritoferreira@hotmail.com>
2026-08-10 14:35:34 -0300

    feat: add gameService class with constructor and getTrendingGames method

---
+++ app/Services/gameService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Services\IGDB\IgdbClient;
+
+final class gameService
+{
+    public function __construct(
+        private readonly IgdbClient $igdbClient,
+    )
+    {
+
+    }
+
+    public function getTrendingGames(): array
+    {
+        return [];
+    }
+
+
+}
